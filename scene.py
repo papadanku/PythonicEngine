a605d70a34fab91be100d153936fc60cf8187895
papadanku <pauldang2@gmail.com>
2023-03-11 00:09:58 -0700

    Complete Transformations (2)

--- scene.py
+++ scene.py
@@ -21,9 +21,10 @@ class Scene:
         app = self.app
         add = self.add_object
 
-        add(Cube(app))
-        add(Cube(app, tex_id=1, pos=(-2.5, 0, 0), rot=(45, 0, 0), scale=(1, 2, 1)))
-        add(Cube(app, tex_id=2, pos=(2.5, 0, 0), rot=(0, 0, 45), scale=(1, 1, 2)))
+        n, s, = 30, 3 # Number, step
+        for x in range(-n, n, s):
+            for z in range(-n, n, s):
+                add(Cube(app, pos=(x, -s, z)))
 
     def render(self):
         for obj in self.objects:
