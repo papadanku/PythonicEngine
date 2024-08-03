0e39cfe909a88a1bd059dcb278e0e803df05827f
papadanku <115061077+papadanku@users.noreply.github.com>
2024-08-03 12:18:52 -0700

    Cleanups

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,9 @@ author = 'papadanku'
 extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
