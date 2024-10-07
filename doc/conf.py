7a1f2cf357760be0e2d96cad37301cb15e789e0c
papadanku <115061077+papadanku@users.noreply.github.com>
2024-10-07 10:35:36 -0700

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -41,10 +41,6 @@ html_sidebars = {
 html_theme_options = {
     'fixed_sidebar': 'true',
     'show_relbar_bottom': 'true',
-    'page_width': '1200px',
-    'sidebar_width': '400px',
-    'code_font_family': 'Noto Sans Mono',
-    'font_family': 'Noto Sans',
-    'head_font_family': 'Noto Sans',
-    'caption_font_family': 'Noto Sans'
+    'page_width': '80rem',
+    'sidebar_width': '20rem'
 }
